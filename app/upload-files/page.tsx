55e2b157478dd16df04c111e26c57d82da919434
Anirudh-rb26 <anirudhjayakumar.business@gmail.com>
2024-10-06 08:33:26 +0530

    Initial Commit

---
+++ app/upload-files/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const UploadFiles = () => {
+    return (
+        <main className='min-h-screen flex flex-col items-center p-4'>
+            {/* Navigation Buttons */}
+            <div className='flex'>
+                <Button>Navigate back to home</Button>
+            </div>
+        </main>
+    )
+}
+
+export default UploadFiles
